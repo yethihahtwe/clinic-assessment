d9f5c612f4ff0c12a1c566846707c618b2b1cbbe
Ye Thi Ha Htwe <yethihahtwe319@gmail.com>
2024-07-02 23:16:34 +0700

    to continue adding from domain 6;

--- app/Services/FormComponents/FormFields.php
+++ app/Services/FormComponents/FormFields.php
@@ -3,6 +3,7 @@
 namespace App\Services\FormComponents;
 
 use Filament\Forms\Get;
+use Illuminate\Support\Str;
 use App\Services\AssessmentService;
 use Filament\Forms\Components\Tabs;
 use Filament\Forms\Components\Group;
@@ -54,6 +55,10 @@ class FormFields
                 ->tabs([
                     Tab::make(static::getDomainName(1))->schema(static::getDomainOneComponents())->columns(2),
                     Tab::make(static::getDomainName(2))->schema(static::getDomainTwoComponents())->columns(2),
+                    Tab::make(static::getDomainName(3))->schema(static::getDomainWithSubdomainBooleanOnly(3))->columns(2),
+                    Tab::make(static::getDomainName(4))->schema(static::getDomainWithSubdomainBooleanOnly(4))->columns(2),
+                    Tab::make(static::getDomainName(5))->schema(static::getDomainWithSubdomainBooleanOnly(5))->columns(2),
+                    Tab::make(static::getDomainName(6))->schema(static::getDomainWithSubdomainBooleanOnly(6))->columns(2),
                 ])
                 ->columnSpanFull()
             // Section::make('Responses')->schema([Tabs::make()->tabs(AssessmentService::schema())]),
@@ -88,10 +93,15 @@ class FormFields
             foreach ($questions as $question) {
                 $questionLabel = $question->name;
                 $questionSlug = $question->slug;
-                $questionComponents[] = Radio::make('choices.'. $questionSlug)->label($i . '. ' . $questionLabel)->boolean()->inline()->inlineLabel(false)->live();
-                $questionComponents[] = Group::make([
-                    TextInput::make('choices.'. $questionSlug. '.m')->label('Male')->visible(fn (Get $get) => $get('choices.'. $questionSlug) == 1),
-                ]);
+                $questionComponents[] = Radio::make('choices.'. $questionSlug)->label($i . '. ' . $questionLabel)->boolean()->inline()->inlineLabel(false)->live()->columnSpan(1);
+                $questionComponents[] = Fieldset::make($questionSlug . '-count')
+                ->label($questionLabel . ' count')
+                ->schema([
+                    self::hrCountInput('m', $questionSlug),
+                    self::hrCountInput('f', $questionSlug),
+                    self::hrCountInput('o', $questionSlug),
+                ])->columnSpan(1)->columns(3)
+                ->visible(fn (Get $get) => $get('choices.' . $questionSlug) == 1);
                 $i++;
             }
             $subdomainComponents [] = Fieldset::make($subdomainLabel)->schema($questionComponents);
@@ -100,8 +110,43 @@ class FormFields
         return $subdomainComponents;
     }
 
+    protected static function getDomainWithSubdomainBooleanOnly($domainId): array
+    {
+        $subdomains = \App\Models\Subdomain::where('domain_id', $domainId)->get();
+        $subdomainComponents = [];
+        foreach ($subdomains as $subdomain) {
+            $subdomainId = $subdomain->id;
+            $subdomainLabel = $subdomain->name;
+
+            $questions = \App\Models\Question::where('subdomain_id', $subdomainId)->get();
+            $questionComponents = [];
+            $i = 1;
+            foreach ($questions as $question) {
+                $questionLabel = $question->name;
+                $questionSlug = $question->slug;
+                $questionComponents[] = Radio::make('choices.'. $questionSlug)->label($i. '. '. $questionLabel)->boolean()->inline()->inlineLabel(false)->columnSpan(1);
+                $i++;
+            }
+            $subdomainComponents [] = Fieldset::make($subdomainLabel)->schema($questionComponents);
+        }
+        return $subdomainComponents;
+    }
+
+
     protected static function getDomainName(int $domainId): string
     {
         return \App\Models\Domain::find($domainId)->name;
+    }
+
+    protected static function hrCountInput($g, $questionSlug, ): TextInput
+    {
+        $label = $g =='m'? 'Male' : ($g == 'f'? 'Female' : 'Other');
+        return TextInput::make('choices.' . $questionSlug . '-' . $g)
+        ->label($label)
+        ->placeholder('Count')
+        ->numeric()
+        ->default(0)
+        ->minValue(0)
+        ->suffix('people');
     }
 }
